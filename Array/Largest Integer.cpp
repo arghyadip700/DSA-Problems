ccc02dfdd85343bcfe0f3df4b855628324d4afdc
Arghyadip Dhara <arghyadipdhara1@gmail.com>
2023-07-08 11:40:59 +0530

    Largest Integer Added

---
+++ Array/Largest Integer.cpp
@@ -0,0 +1,18 @@
+/*Given an array A[] of size n. The task is to find the largest element in it.*/
+
+// Solution:
+
+/*
+int largest(vector<int> &arr, int n)
+{
+    int max = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+*/
